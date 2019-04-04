bfc8c5238b35eeb136d856e97a750239ecb8ba00
Kumar Akshay <k.akshay9721@gmail.com>
2019-04-05 00:07:59 +0530

    update schema

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,15 +3,15 @@ from django.db import models
 
 # Create your models here.
 class Equipment(models.Model):
-    equip_name = models.CharField(max_length=30)
-    equip_id = models.CharField(max_length=30)
-    equip_rating = models.IntegerField()
+    name = models.CharField(max_length=30)
+    rating = models.IntegerField()
+    priority = models.IntegerField() # higher is more important
 
 
 class User(models.Model):
-    user_name = models.CharField(max_length=30)
-    user_password = models.CharField(max_length=30)
-    user_rights = models.IntegerField()  # admin - 1, noobie - 0
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    rights = models.IntegerField()  # admin - 1, noobie - 0
 
 class Usage(models.Model):
     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
