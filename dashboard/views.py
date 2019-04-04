4052e3f0d02800180329727c427a16dbe30f51a2
raghav9-97 <raghu2288661@gmail.com>
2019-04-05 00:46:05 +0530

    Fixed routing dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def dashboard(request):
     template_name = 'dashboard.html'
     return render(request, template_name, {})
+
+def index(request):
+    index_name = 'index.html'
+    return render(request, index_name, {})
--- iot_dashboard/urls.py
+++ iot_dashboard/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from dashboard.views import dashboard
+from dashboard.views import dashboard,index
 from dashboard.api import EquipmentResource
 from django.contrib import admin
 from django.urls import include, path
@@ -25,5 +25,6 @@ v1_api.register(EquipmentResource())
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dash/', dashboard),
+    path('index/', index),
     path('api/', include(v1_api.urls)),
 ]
